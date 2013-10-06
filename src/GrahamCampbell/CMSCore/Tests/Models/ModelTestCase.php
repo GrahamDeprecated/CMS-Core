f06d50de2b10a101734c8a570d74820eb2471021
Graham Campbell <graham@mineuk.com>
2013-10-06 21:11:35 +0100

    Frrrrrrr

--- src/GrahamCampbell/CMSCore/Tests/Models/ModelTestCase.php
+++ src/GrahamCampbell/CMSCore/Tests/Models/ModelTestCase.php
@@ -34,12 +34,9 @@ abstract class ModelTestCase extends TestCase {
     public function setUp() {
         parent::setUp();
 
-        Artisan::call('migrate', array(
-            '--path' => 'src/migrations', 
-            '--database' => 'testbench'
-        ));
+        Artisan::call('migrate', array('--package' => 'graham-campbell/cms-core'));
 
-        Artisan::call('db:seed',  array('--class' => 'GrahamCampbell\CMSCore\Seeds\DatabaseSeeder'));
+        Artisan::call('db:seed', array('--class' => 'GrahamCampbell\CMSCore\Seeds\DatabaseSeeder'));
 
         $this->object = new $this->model;
         $this->instance = $this->object->find(1);
