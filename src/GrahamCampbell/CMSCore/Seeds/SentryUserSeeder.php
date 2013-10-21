3aa1c82d155478e3f46af0b8be0077bb5b3d3670
Graham Campbell <graham@mineuk.com>
2013-10-21 22:40:24 +0100

    Updated user seeding

--- src/GrahamCampbell/CMSCore/Seeds/SentryUserSeeder.php
+++ src/GrahamCampbell/CMSCore/Seeds/SentryUserSeeder.php
@@ -20,6 +20,8 @@
  * @link       https://github.com/GrahamCampbell/CMS-Core
  */
 
+use DateTime;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Cartalyst\Sentry\Facades\Laravel\Sentry;
@@ -35,56 +37,62 @@ class SentryUserSeeder extends Seeder {
         DB::table('users')->delete();
 
         $user = array(
-            'first_name' => 'CMS',
-            'last_name'  => 'Admin',
-            'email'      => 'admin@dsmg.co.uk',
-            'password'   => 'password',
-            'activated'  => 1,
+            'first_name'   => 'CMS',
+            'last_name'    => 'Admin',
+            'email'        => 'admin@dsmg.co.uk',
+            'password'     => 'password',
+            'activated'    => 1,
+            'activated_at' => new DateTime
         );
         Sentry::getUserProvider()->create($user);
 
         $user = array(
-            'first_name' => 'CMS',
-            'last_name'  => 'Semi-Admin',
-            'email'      => 'semiadmin@dsmg.co.uk',
-            'password'   => 'password',
-            'activated'  => 1,
+            'first_name'   => 'CMS',
+            'last_name'    => 'Semi-Admin',
+            'email'        => 'semiadmin@dsmg.co.uk',
+            'password'     => 'password',
+            'activated'    => 1,
+            'activated_at' => new DateTime
         );
         Sentry::getUserProvider()->create($user);
 
         $user = array(
-            'first_name' => 'CMS',
-            'last_name'  => 'Moderator',
-            'email'      => 'moderator@dsmg.co.uk',
-            'password'   => 'password',
-            'activated'  => 1,
+            'first_name'   => 'CMS',
+            'last_name'    => 'Moderator',
+            'email'        => 'moderator@dsmg.co.uk',
+            'password'     => 'password',
+            'activated'    => 1,
+            'activated_at' => new DateTime
         );
         Sentry::getUserProvider()->create($user);
 
         $user = array(
-            'first_name' => 'CMS',
-            'last_name'  => 'Blogger',
-            'email'      => 'blogger@dsmg.co.uk',
-            'password'   => 'password',
-            'activated'  => 1,
+            'first_name'   => 'CMS',
+            'last_name'    => 'Blogger',
+            'email'        => 'blogger@dsmg.co.uk',
+            'password'     => 'password',
+            'activated'    => 1,
+            'activated_at' => new DateTime
         );
         Sentry::getUserProvider()->create($user);
 
         $user = array(
-            'first_name' => 'CMS',
-            'last_name'  => 'Editor',
-            'email'      => 'editor@dsmg.co.uk',
-            'password'   => 'password',
-            'activated'  => 1,
+            'first_name'   => 'CMS',
+            'last_name'    => 'Editor',
+            'email'        => 'editor@dsmg.co.uk',
+            'password'     => 'password',
+            'activated'    => 1,
+            'activated_at' => new DateTime
         );
         Sentry::getUserProvider()->create($user);
 
         $user = array(
-            'first_name' => 'CMS',
-            'last_name'  => 'User',
-            'email'      => 'user@dsmg.co.uk',
-            'password'   => 'password',
-            'activated'  => 1,
+            'first_name'   => 'CMS',
+            'last_name'    => 'User',
+            'email'        => 'user@dsmg.co.uk',
+            'password'     => 'password',
+            'activated'    => 1,
+            'activated_at' => new DateTime
         );
         Sentry::getUserProvider()->create($user);
     }
