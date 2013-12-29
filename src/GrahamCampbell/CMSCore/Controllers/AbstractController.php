df00208a1f89b0cf12c493a66c59ea7c0f6e55e7
Graham Campbell <graham@mineuk.com>
2013-12-29 12:12:12 +0000

    Port to my credentials package

--- src/GrahamCampbell/CMSCore/Controllers/AbstractController.php
+++ src/GrahamCampbell/CMSCore/Controllers/AbstractController.php
@@ -16,12 +16,7 @@
 
 namespace GrahamCampbell\CMSCore\Controllers;
 
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\View;
-use Cartalyst\Sentry\Facades\Laravel\Sentry;
-use GrahamCampbell\Navigation\Facades\Navigation;
-use GrahamCampbell\CMSCore\Facades\PageProvider;
-use GrahamCampbell\Core\Controllers\AbstractController as Controller;
+use GrahamCampbell\Credentials\Controllers\AbstractController as Controller;
 
 /**
  * This is the abstract controller class.
@@ -35,39 +30,18 @@ use GrahamCampbell\Core\Controllers\AbstractController as Controller;
 abstract class AbstractController extends Controller
 {
     /**
-     * A list of methods protected by user permissions.
-     *
-     * @var array
-     */
-    private $users = array();
-
-    /**
      * A list of methods protected by edit permissions.
      *
      * @var array
      */
-    private $edits = array();
+    protected $edits = array();
 
     /**
      * A list of methods protected by blog permissions.
      *
      * @var array
      */
-    private $blogs = array();
-
-    /**
-     * A list of methods protected by mod permissions.
-     *
-     * @var array
-     */
-    private $mods = array();
-
-    /**
-     * A list of methods protected by admin permissions.
-     *
-     * @var array
-     */
-    private $admins = array();
+    protected $blogs = array();
 
     /**
      * Constructor (setup protection and permissions).
@@ -76,53 +50,9 @@ abstract class AbstractController extends Controller
      */
     public function __construct()
     {
-        $this->beforeFilter('csrf', array('on' => 'post'));
+        parent::__construct();
 
-        Sentry::getThrottleProvider()->enable();
-
-        $this->beforeFilter('auth:user', array('only' => $this->users));
         $this->beforeFilter('auth:edit', array('only' => $this->edits));
         $this->beforeFilter('auth:blog', array('only' => $this->blogs));
-        $this->beforeFilter('auth:mod', array('only' => $this->mods));
-        $this->beforeFilter('auth:admin', array('only' => $this->admins));
-    }
-
-    /**
-     * Set the permission.
-     *
-     * @pram  string  $action
-     * @pram  string  $permission
-     * @return void
-     */
-    protected function setPermission($action, $permission)
-    {
-        $this->{$permission.'s'}[] = $action;
-    }
-
-    /**
-     * Set the permissions.
-     *
-     * @pram  array  $permissions
-     * @return void
-     */
-    protected function setPermissions($permissions)
-    {
-        foreach ($permissions as $action => $permission) {
-            $this->setPermission($action, $permission);
-        }
-    }
-
-    /**
-     * Set the user id.
-     *
-     * @return int
-     */
-    protected function getUserId()
-    {
-        if (Sentry::getUser()) {
-            return Sentry::getUser()->getId();
-        } else {
-            return 1;
-        }
     }
 }
